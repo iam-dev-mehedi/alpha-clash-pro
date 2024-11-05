675b2ea3642139f1c191163302acd51fb5ee7610
iam-dev-mehedi <iam.dev.mehedi@gmail.com>
2024-11-05 23:24:34 +0600

    procesing score button and life button

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -10,14 +10,57 @@
 //     // console.log(playgroundSection.classList);
 // }
 
+
+// function handleKeyboardButtonPress(){
+//     console.log('button presed');
+// }
+// // Captured keyboard key press
+// document.addEventListener('keyup', handleKeyboardButtonPress);
+
+function handleKeyboardKeyUpEvent(event){
+    const playerPressed = event.key;
+    console.log('player Pressed', playerPressed);
+
+    // get the expected to press
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    const currentAlphabet = currentAlphabetElement.innerText;
+    const expectedAlphabet = currentAlphabet.toLowerCase();
+    // console.log(playerPressed, expectedAlphabet);
+
+    // cheak matched or not,  right or wrong key press
+    if(playerPressed === expectedAlphabet){
+        console.log('to get a point');
+        // update score
+        // 1. get the current score
+        const currentScoreElement = document.getElementById('current-score');
+
+        // 2. increase the score by 1
+        // 3. show the updated score
+
+        // start a new round
+        console.log('you have preesed correctly', expectedAlphabet);
+        removeBackgroundColorById(expectedAlphabet);
+        continueGame();
+    }
+    else{
+        console.log('you missed. you lost a life');
+    }
+}
+
+
+document.addEventListener('keyup', handleKeyboardKeyUpEvent);
+
+
+
+
 function continueGame(){
     // step-1: generate a random alphabet
     const alphabet = getARandomAlphabet();
-    console.log('your random alphabet', alphabet);
+    // console.log('your random alphabet', alphabet);
     
     // set randomly generated alphabet to the screen (show it)
-    const crrrentAlphabetElement = document.getElementById('current-alphabet');
-    crrrentAlphabetElement.innerText = alphabet;
+    const currentAlphabetElement = document.getElementById('current-alphabet');
+    currentAlphabetElement.innerText = alphabet;
 
     // set background color
     setBackgroundColorById(alphabet);

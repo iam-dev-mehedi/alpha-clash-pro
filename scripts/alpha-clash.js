de23474f99f8c769abdb3a8d19d370065a2f0562
iam-dev-mehedi <iam.dev.mehedi@gmail.com>
2024-11-06 22:51:33 +0600

    finaly complete the unleash alpha project

--- scripts/alpha-clash.js
+++ scripts/alpha-clash.js
@@ -21,7 +21,13 @@ function handleKeyboardKeyUpEvent(event){
     const playerPressed = event.key;
     console.log('player Pressed', playerPressed);
 
-    // get the expected to press
+
+    // stop the game if pressed 'Esc'
+    if(playerPressed === 'Escape'){
+        gameOver();
+    }
+
+    // key player is expected to press
     const currentAlphabetElement = document.getElementById('current-alphabet');
     const currentAlphabet = currentAlphabetElement.innerText;
     const expectedAlphabet = currentAlphabet.toLowerCase();
@@ -30,20 +36,54 @@ function handleKeyboardKeyUpEvent(event){
     // cheak matched or not,  right or wrong key press
     if(playerPressed === expectedAlphabet){
         console.log('to get a point');
+
+        const currentScore = getTextElementValueById('current-score');
+        const updatedScore = currentScore + 1;
+        setTextElementValueById('current-score', updatedScore);
+
+
+
+
+        // -----------------------------------
         // update score
         // 1. get the current score
-        const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreElement = document.getElementById('current-score');
+        // const currentScoreText = currentScoreElement.innerText;
+        // const currentScore = parseInt(currentScoreText);
+        // console.log(currentScore);
 
-        // 2. increase the score by 1
-        // 3. show the updated score
+        // // 2. increase the score by 1
+        // const newScore = currentScore + 1;
+        // // 3. show the updated score
+        // currentScoreElement.innerText = newScore;
 
-        // start a new round
-        console.log('you have preesed correctly', expectedAlphabet);
+        // // start a new round
+        // console.log('you have preesed correctly', expectedAlphabet);
         removeBackgroundColorById(expectedAlphabet);
         continueGame();
     }
     else{
         console.log('you missed. you lost a life');
+
+        const currentLife = getTextElementValueById('current-life');
+        const updatedLife = currentLife - 1;
+        setTextElementValueById('current-life', updatedLife);
+
+        // step-1: get the current life number
+        // const currentLifeElement = document.getElementById('current-life');
+        // const currentLifeText = currentLifeElement.innerText;
+        // const currentLife = parseInt(currentLifeText);
+
+        // // step-2: reduce the life count
+        // const newLife = currentLife - 1;
+
+        // // step-3: display the updated life count
+        // currentLifeElement.innerText = newLife;
+
+
+        if(updatedLife === 0){
+            gameOver();
+        }
     }
 }
 
@@ -68,7 +108,31 @@ function continueGame(){
 }
 
 function play(){
+    // hide everything show only the playground
     hideElementById('home-screen');
+    hideElementById('final-score')
     showElementById('play-ground');
+
+    // reset score and life
+    setTextElementValueById('current-life', 5);
+    setTextElementValueById('current-score', 0);
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    // update final score
+    // 1. get the final score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score', lastScore);
+
+    // clear the last selected alphabet highlight
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
+
 }
